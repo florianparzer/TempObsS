1ecdc5c1ea459defe03f401780d880593d2b7540
Florian <florian-parzer@gmx.at>
2019-03-13 13:22:02 +0100

    Verbesserungen des connectivitysensors-skript damit sensoren, die keinen Wert liefern auch offline gesetzt werden

--- skripts/connectivitysensors.py
+++ skripts/connectivitysensors.py
@@ -50,26 +50,29 @@ except Exception as e:
 
 
 while True:
+    sens = []
     cur.execute('select fk_sensorID, max(zeit) from messung group by fk_sensorID;')
     results = cur.fetchall()
     logging.info(results)
     for result in results:
+        sensor = result[0]
+        sens.append(sensor)
         now = datetime.now()
         logging.info(str(now))
         date = datetime.strptime(str(result[1]),"%Y-%m-%d %H:%M:%S")
         logging.info(date)
-        cur.execute('select status from sensor where sensorID = ' + str(result[0]))
+        cur.execute('select status from sensor where sensorID = ' + str(sensor))
         status = cur.fetchall()
         status = status[0][0]
         logging.info(status)
         if now - timedelta(minutes=2) > date and status == "online":
             #update db
             logging.info("update offline")
-            cur.execute('update sensor set status = \'offline\' where sensorID = ' + str(result[0]))
+            cur.execute('update sensor set status = \'offline\' where sensorID = ' + str(sensor))
 
             #sms versenden
             with open("/var/spool/sms/outgoing/sensor_offline.txt", mode='w') as f:
-                cur.execute('select sensorPosition from sensor where sensorID = ' + str(result[0]))
+                cur.execute('select sensorPosition from sensor where sensorID = ' + str(sensor))
                 sensorPosition = cur.fetchall()
                 sensorPosition = sensorPosition[0][0]
                 print(To + "\nSensor " + sensorPosition + ": keine Messungen mehr", file=f)
@@ -77,15 +80,19 @@ while True:
         elif now - timedelta(minutes=1) < date and status == "offline":
             logging.info("update online")
             #update db
-            cur.execute('update sensor set status = \'online\' where sensorID = ' + str(result[0]))
+            cur.execute('update sensor set status = \'online\' where sensorID = ' + str(sensor))
 
             #sms versenden
             with open("/var/spool/sms/outgoing/sensor_offline.txt", mode='w') as f:
-                cur.execute('select sensorPosition from sensor where sensorID = ' + str(result[0]))
+                cur.execute('select sensorPosition from sensor where sensorID = ' + str(sensor))
                 sensorPosition = cur.fetchall()
                 sensorPosition = sensorPosition[0][0]
                 print(To + "\nSensor " + sensorPosition + ": wieder online", file=f)
+
+    cur.execute('select pk_sensorID from sensor;')
+    results = cur.fetchall()
+    for i in results:
+        if i[0] not in sens:
+            cur.execute('update sensor set status = \'offline\' where sensorID = ' + str(i[0]))
     logging.info("fertig")
     time.sleep(10)
-
-
